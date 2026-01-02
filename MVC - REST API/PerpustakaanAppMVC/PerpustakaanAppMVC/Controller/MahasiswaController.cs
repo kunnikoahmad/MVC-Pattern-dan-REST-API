47446fb1101464bef8c29a1813f2e3da1eac0921
Kunniko <kunnikoahmad@students.amikom.ac.id>
2026-01-02 10:15:12 +0700

    first commit

---
+++ MVC - REST API/PerpustakaanAppMVC/PerpustakaanAppMVC/Controller/MahasiswaController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+using PerpustakaanAppMVC.Model.Entity;
+using PerpustakaanAppMVC.Model.Repository;
+using PerpustakaanAppMVC.Model.Context;
+using System.Windows.Forms;
+
+namespace PerpustakaanAppMVC.Controller
+{
+    public class MahasiswaController
+    {
+        // deklarasi objek Repository untuk menjalankan operasi CRUD
+        private MahasiswaRepository _repository;
+
+        /// <summary>
+        /// Method untuk menampilkan data mahasiwa berdasarkan nama
+        /// </summary>
+        /// <param name="nama"></param>
+        /// <returns></returns>
+        public List<Mahasiswa> ReadByNama(string nama)
+        {
+            // membuat objek collection
+            List<Mahasiswa> list = new List<Mahasiswa>();
+
+            // membuat objek context menggunakan blok using
+            var repo = new MahasiswaRestApiRepository();
+            list = repo.ReadByNama(nama);
+                
+            return list;
+        }
+
+        /// <summary>
+        /// Method untuk menampilkan semua data mahasiswa
+        /// </summary>
+        /// <returns></returns>
+        public List<Mahasiswa> ReadAll()
+        {
+            // membuat objek collection
+            List<Mahasiswa> list = new List<Mahasiswa>();
+
+            // membuat objek context menggunakan blok using
+            var repo = new MahasiswaRestApiRepository();
+            list = repo.ReadAll();
+
+            return list;
+        }
+
+        public int Create(Mahasiswa mhs)
+        {
+            int result = 0;
+
+            // cek npm yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Npm))
+            {
+                throw new ArgumentException("NPM harus diisi !!!");
+            }
+
+            // cek nama yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Nama))
+            {
+                throw new ArgumentException("Nama harus diisi !!!");
+            }
+
+            // cek angkatan yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Angkatan))
+            {
+                throw new ArgumentException("Angkatan harus diisi !!!");
+            }
+
+            // membuat objek context menggunakan blok using
+            var repo = new MahasiswaRestApiRepository();
+            result = repo.Create(mhs);
+
+            if (result > 0)
+            {
+                MessageBox.Show("Data mahasiswa berhasil disimpan !", "Informasi",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Data mahasiswa gagal disimpan !!!", "Peringatan",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            return result;
+        }
+
+        public int Update(Mahasiswa mhs)
+        {
+            int result = 0;
+
+            // cek npm yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Npm))
+            {
+                throw new ArgumentException("NPM harus diisi !!!");
+            }
+
+            // cek nama yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Nama))
+            {
+                throw new ArgumentException("Nama harus diisi !!!");
+            }
+
+            // cek angkatan yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Angkatan))
+            {
+                throw new ArgumentException("Angkatan harus diisi !!!");
+            }
+
+            // membuat objek context menggunakan blok using
+            var repo = new MahasiswaRestApiRepository();
+            result = repo.Update(mhs);
+
+            return result;
+        }
+
+        public int Delete(Mahasiswa mhs)
+        {
+            int result = 0;
+
+            // cek nilai npm yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Npm))
+            {
+                throw new ArgumentException("NPM harus diisi !!!");
+            }
+
+            // membuat objek context menggunakan blok using
+            var repo = new MahasiswaRestApiRepository();
+            result = repo.Delete(mhs);
+
+            return result;
+        }
+    }
+}
---
+++ MVC - REST API/PerpustakaanAppMVC/PerpustakaanAppMVC/Model/Repository/MahasiswaRestApiRepository.cs
@@ -0,0 +1,99 @@
+﻿using PerpustakaanAppMVC.Model.Entity;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+
+namespace PerpustakaanAppMVC.Model.Repository
+{
+    internal class MahasiswaRestApiRepository
+    {
+        public int Create(Mahasiswa mhs)
+        {
+            string baseUrl = "http://latihan.coding4ever.net:5555/";
+            string endpoint = "api/mahasiswa";
+
+            var client = new RestClient(baseUrl);
+
+            var request = new RestRequest(endpoint, Method.POST);
+
+            request.AddJsonBody(mhs);
+
+            var response = client.Execute(request);
+
+            var result = response.Content.IndexOf("1") > 0 ? 1 : 0;
+
+            return result;
+        }
+
+        public int Update(Mahasiswa mhs)
+        {
+            string baseUrl = "http://latihan.coding4ever.net:5555/";
+            string endpoint = "api/mahasiswa/{npm}";
+
+            var client = new RestClient(baseUrl);
+
+            var request = new RestRequest(endpoint, Method.PUT);
+            request.AddUrlSegment("npm", mhs.Npm);
+
+            request.AddJsonBody(mhs);
+
+            var response = client.Execute(request);
+
+            int result = response.IsSuccessful ? 1 : 0;
+
+            return result;
+        }
+
+        public int Delete(Mahasiswa mhs)
+        {
+         
+            string baseUrl = "http://latihan.coding4ever.net:5555/";
+            string endpoint = "api/mahasiswa/{npm}";
+
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest(endpoint, Method.DELETE);
+
+            request.AddUrlSegment("npm", mhs.Npm);
+
+            var response = client.Execute(request);
+
+            int result = response.IsSuccessful ? 1 : 0;
+            return result;
+        
+        }
+
+        public List<Mahasiswa> ReadAll()
+        {
+            string baseUrl = "http://latihan.coding4ever.net:5555/";
+            string endpoint = "api/mahasiswa";
+
+            var client = new RestClient(baseUrl);
+
+            var request = new RestRequest(endpoint, Method.GET);
+
+            var response = client.Execute<List<Mahasiswa>>(request);
+
+            return response.Data;
+        }
+
+        public List<Mahasiswa> ReadByNama(string nama)
+        {
+            string baseUrl = "http://latihan.coding4ever.net:5555/";
+            string endpoint = "api/mahasiswa";
+
+            var client = new RestClient(baseUrl);
+
+            var request = new RestRequest(endpoint, Method.GET);
+            request.AddParameter("nama",nama);
+
+            var response = client.Execute<List<Mahasiswa>>(request);
+
+            return response.Data;
+        }
+
+        public Mahasiswa ReadByNpm(string npm)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
